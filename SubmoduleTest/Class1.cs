bfab07d00265da5bc0ad93850a0a0acebde1df57
rdosen <roman.dosen@gmail.com>
2022-04-19 21:20:27 +0200

    Add Class2

--- SubmoduleTest/Class1.cs
+++ SubmoduleTest/Class1.cs
@@ -2,7 +2,15 @@
 
 namespace SubmoduleTest
 {
+	/// <summary>
+	/// This is test class
+	/// </summary>
 	public class Class1
 	{
+		
+		public Class1()
+		{
+			SnmpSharpNet.AgentParameters agentParameters = new SnmpSharpNet.AgentParameters();
+		}
 	}
 }
